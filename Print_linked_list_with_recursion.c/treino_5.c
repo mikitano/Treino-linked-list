6f48930c71afa33bccc888731e65e691da6dcc32
Magali Midori Kitano <mkitano@c2r8p2.42sp.org.br>
2025-10-23 20:06:39 -0300

    print linked list w/ recursion

---
+++ Print_linked_list_with_recursion.c/treino_5.c
@@ -0,0 +1,45 @@
+#include "treino.h"
+
+void	ft_print_recursion(t_node *head)
+{
+	t_node	*temp;
+
+	if (head == NULL)
+		return ;
+	temp = head;
+	printf("%d",temp->data);
+	ft_print_recursion(temp->next);
+}
+
+void	ft_insert(int i, t_node **head)
+{
+	t_node	*new;
+	t_node	*temp;
+
+	new = malloc(sizeof(t_node));
+	if (!new)
+		return ;
+	new->data = i;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+}
+
+int	main(void)
+{
+	t_node	*head;
+
+	head = NULL;
+	ft_insert(2, &head);
+	ft_insert(4, &head);
+	ft_insert(6, &head);
+	ft_insert(5, &head);
+	ft_print_recursion(head);
+}
