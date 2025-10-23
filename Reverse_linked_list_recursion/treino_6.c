0409f188ef4133095ce6aca0a2759e4ac6147ba0
Magali Midori Kitano <mkitano@c2r8p2.42sp.org.br>
2025-10-23 20:38:53 -0300

    start recursion reverse list

--- Reverse_linked_list_recursion/treino_6.c
+++ Reverse_linked_list_recursion/treino_6.c
@@ -1,2 +1,24 @@
 #include "treino.h"
 
+void	ft_reverse_print_recurusion(t_node *head)
+{
+	t_node	*temp;
+
+	if (head == NULL)
+		return ;
+	ft_reverse_print_recurusion(temp->next);
+	printf("%d", temp->data);
+}
+
+void	ft_reverse_list(t_node **head)
+{
+	t_node	*temp;
+
+	temp = *head;
+	if (temp->next == NULL)
+	{
+		head = temp;
+		return ;
+	}
+	ft_reverse_list(head->next);
+}
