aa9aff1cae739168701b4c77ccc71df150a9ef46
magakitano <magakitano@yahoo.com.br>
2025-10-26 18:53:28 -0300

    not finished

--- Doubly linked list/treino_7.c
+++ Doubly linked list/treino_7.c
@@ -1,8 +1,33 @@
 #include "treino.h"
 
-void	insert_at_head(t_node **head)
+t_node	*create_new_node(int i)
 {
+	t_node	*new_node;
 	
+	new_node = malloc(sizeof(t_node));
+	new_node->data = i;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	insert_at_head(int i, t_node **head)
+{
+	t_node	*newNode;
+	t_node	temp;
+
+	new_node = create_new_node(i);
+	if (head == NULL)
+	{
+		head = newNode;
+		return ;
+	}
+	temp
+	else
+	{
+		while (temp)
+
+	}
 }
 
 int	main (void)
